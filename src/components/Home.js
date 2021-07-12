b7150108672453c93f76afbeed3bad03b95ce6be
bharat_patil <patilbharat09@gmail.com>
2021-07-12 19:58:43 +0530

    pass date to hero image component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import { POPULAR_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 // Components
+import HeroImage from './HeroImage';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -13,9 +14,20 @@ import NoImage from '../images/no_image.jpg'
 
 const Home = () => {
 
-  const {state, loading, error } = useHomeFetch();
-  
-  return <div>Home Page</div>
+  const { state, loading, error } = useHomeFetch();
+
+  return (
+    <>
+      {state.results[0] ?
+        <HeroImage 
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+        : null
+      }
+    </>
+  );
 }
 
 export default Home;
