38523060476aea3d055c2b702f4c9578d7cb33fd
bharat_patil <patilbharat09@gmail.com>
2021-07-13 19:03:51 +0530

    Added load more button component in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
 import SearchBar from './SearchBar';
+import Button from "./Button";
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -23,15 +24,15 @@ const Home = () => {
   return (
     <>
       {!searchTerm && state.results[0] ?
-        <HeroImage 
+        <HeroImage
           image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
           title={state.results[0].original_title}
           text={state.results[0].overview}
         />
         : null
       }
-      <SearchBar setSearchTerm={setSearchTerm}/>
-      <Grid header={ searchTerm ? "Search Result" : "Popular Movies"}>
+      <SearchBar setSearchTerm={setSearchTerm} />
+      <Grid header={searchTerm ? "Search Result" : "Popular Movies"}>
         {state.results.map(movie => (
           <Thumb
             key={movie.id}
@@ -43,7 +44,10 @@ const Home = () => {
           />
         ))}
       </Grid>
-      <Spinner/>
+      {loading && <Spinner />}
+      {state.page < state.total_pages && !loading && (
+        <Button text="Load More" />
+      )}
     </>
   );
 }
