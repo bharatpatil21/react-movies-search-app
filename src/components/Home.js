44b96981b20c542cf5dd134dfb8a3b558d94f002
bharat_patil <patilbharat09@gmail.com>
2021-07-13 16:13:37 +0530

    Addedsearch bar in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ import HeroImage from './HeroImage';
 import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
+import SearchBar from './SearchBar';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -17,7 +18,7 @@ import NoImage from '../images/no_image.jpg'
 
 const Home = () => {
 
-  const { state, loading, error } = useHomeFetch();
+  const { state, loading, error, setSearchTerm } = useHomeFetch();
 
   return (
     <>
@@ -29,6 +30,7 @@ const Home = () => {
         />
         : null
       }
+      <SearchBar setSearchTerm={setSearchTerm}/>
       <Grid header="Popular Movies">
         {state.results.map(movie => (
           <Thumb
